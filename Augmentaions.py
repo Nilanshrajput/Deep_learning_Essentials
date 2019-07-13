72e1c69026e8266f66c2dffd42fdab0cc3330bb5
Nilansh Rajput <rajputnilansh@gmail.com>
2019-07-13 12:10:19 +0530

    Augmentaions file addes

---
+++ Augmentaions.py
@@ -0,0 +1,66 @@
+from albumentations import (
+    PadIfNeeded,
+    HorizontalFlip,
+    VerticalFlip,    
+    CenterCrop,    
+    Crop,
+    Compose,
+    Transpose,
+    RandomRotate90,
+    ElasticTransform,
+    GridDistortion, 
+    OpticalDistortion,
+    RandomSizedCrop,
+    OneOf,
+    CLAHE,
+    RandomBrightnessContrast,    
+    RandomGamma,
+    RGBShift
+)
+
+original_height=224
+original_width=224
+aug = Compose([
+    OneOf([RandomSizedCrop(min_max_height=(50, 101), height=original_height, width=original_width, p=0.5),
+          PadIfNeeded(min_height=original_height, min_width=original_width, p=0.5)], p=1),    
+    VerticalFlip(p=0.5),              
+    RandomRotate90(p=0.5),
+    OneOf([
+        ElasticTransform(p=0.4, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03),
+        GridDistortion(p=0.3),
+        OpticalDistortion(p=0.4, distort_limit=2, shift_limit=0.5)                  
+        ], p=0.8),
+    CLAHE(p=0.8),
+    RandomBrightnessContrast(p=0.8),
+    RGBShift(r_shift_limit=20, g_shift_limit=20, b_shift_limit=20, always_apply=False, p=0.9),
+    RandomGamma(p=0.8)])
+
+def visualize(image, mask, original_image=None, original_mask=None):
+    fontsize = 18
+    
+    if original_image is None and original_mask is None:
+        f, ax = plt.subplots(2, 1, figsize=(8, 8))
+
+        ax[0].imshow(image)
+        ax[1].imshow(mask)
+    else:
+        f, ax = plt.subplots(2, 2, figsize=(8, 8))
+
+        ax[0, 0].imshow(original_image)
+        ax[0, 0].set_title('Original image', fontsize=fontsize)
+        
+        ax[1, 0].imshow(original_mask)
+        ax[1, 0].set_title('Original mask', fontsize=fontsize)
+        
+        ax[0, 1].imshow(image)
+        ax[0, 1].set_title('Transformed image', fontsize=fontsize)
+        
+        ax[1, 1].imshow(mask)
+        ax[1, 1].set_title('Transformed mask', fontsize=fontsize)
+
+augmented = aug(image=image, mask=mask)
+
+image_elastic = augmented['image']
+mask_elastic = augmented['mask']
+
+visualize(image_elastic, mask_elastic, original_image=image, original_mask=mask)

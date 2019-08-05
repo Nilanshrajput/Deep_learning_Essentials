f668933cb8ad5e100255f9820d12706aff8bfd05
Nilansh Rajput <rajputnilansh@gmail.com>
2019-08-05 23:21:02 +0530

    garbage file removed

--- load_intermediate_layer_pytorch.py
+++ load_intermediate_layer_pytorch.py
@@ -1,4 +1,11 @@
 # Load the pretrained model
 model = models.resnet18(pretrained=True)
 # Use the model object to select the desired layer
+
+#created a reference to the layer we want to extract from
 layer = model._modules.get('avgpool')
+
+# Set model to evaluation mode
+
+#n order to ensure that any Dropout layers are not active during the forward pass.
+model.eval()

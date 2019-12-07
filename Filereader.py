2ac1c00acd93b5ec9d23d758d9cec2e7fb3bf5d3
Nilansh Rajput <rajputnilansh@gmail.com>
2019-12-07 21:17:15 +0530

    os.walk

--- Filereader.py
+++ Filereader.py
@@ -2,3 +2,8 @@ mypath = 'F:\weather_deter\Image\cl500'
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 for n in range(1, len(onlyfiles)):
     im = cv2.imread(join(mypath, onlyfiles[n]), 0)
+"""
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+        """
